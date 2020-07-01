cb9d4ab6c21062df5f1af06e86522df19631d63d
Ronaldo Vasconcelos <ronaldo.asconcellos@gmail.com>
2020-07-01 14:44:28 -0300

    first commit

---
+++ Produto.cs
@@ -0,0 +1,39 @@
+using System.Globalization;
+namespace POO
+{
+    public class Produto
+    {
+        public string Nome { get; set; }
+        public double Preco { get; set; }
+        public int Quantidade { get; set; }
+
+        //Constructor
+        public Produto() {
+
+        }
+         public Produto(string nome, double preco, int quantidade) {
+             Nome = nome;
+             Preco = preco;
+             Quantidade = quantidade;
+         }
+
+        public void AdicionarProduto(int quantidade) {
+            Quantidade += quantidade;
+        }
+        public double ValorTotalEmEstoque() {
+                return Preco * Quantidade;
+        }
+        public void RemoverProdutos(int quantidade) {
+            Quantidade -= quantidade;
+        }
+        public override string ToString() {
+            return Nome
+            + ", $"
+            + Preco.ToString("F2", CultureInfo.InstalledUICulture)
+            + ", "
+            + Quantidade
+            + " unidades, Total: $ "
+            + ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace POO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        
+
+           System.Console.WriteLine("Entre com os dados do produto");
+           System.Console.Write("Nome: ");
+           string nome = Console.ReadLine();
+           System.Console.Write("Preco: ");
+           double preco = double.Parse(Console.ReadLine());
+           System.Console.Write("Quantidade: ");
+           int qtd = int.Parse(Console.ReadLine());
+
+           var p = new Produto () {
+               Nome = nome,
+               Preco = preco,
+               Quantidade = qtd
+           };
+
+           System.Console.WriteLine();
+           System.Console.WriteLine($"Dados do produto: {p}");
+        }
+    }
+}
