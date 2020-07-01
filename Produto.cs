68bf35a556017a476bcad06213c34d045989f3cd
Ronaldo Vasconcelos <ronaldo.asconcellos@gmail.com>
2020-07-01 15:02:49 -0300

    alteracao das properts  PRECO E QUANTIDADE

--- Produto.cs
+++ Produto.cs
@@ -4,8 +4,8 @@ namespace POO
     public class Produto
     {
         public string Nome { get; set; }
-        public double Preco { get; set; }
-        public int Quantidade { get; set; }
+        public double Preco { get; private set; }
+        public int Quantidade { get; private set; }
 
         //Constructor
         public Produto() {
